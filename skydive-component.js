9d77f66985bfa2a367af1cf08b015c68d821c406
Aaron Smyth <aas395@gmail.com>
2017-12-18 20:03:03 -0500

    updating skydive component

--- skydive-component.js
+++ skydive-component.js
@@ -1,10 +1,7 @@
 AFRAME.registerComponent('skydive', {
-  init: function() {
-    var position = this.el.getAttribute('position');
-    console.log(position);
-  },
   tick: function () {
     var position = this.el.getAttribute('position');
+    
     if(position.y > 1) {
       this.el.setAttribute('position', {x: position.x, y: position.y - .5, z: position.z});
     }
